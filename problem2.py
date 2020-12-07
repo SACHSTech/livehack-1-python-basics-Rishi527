6aa94da0671f3e8cb73f601a93c564b7c128aa02
Rishi Kalathil <rishi.kalathil23@ycdsbk12.ca>
2020-12-07 18:34:43 +0000

    Completed livehack problem 1 and 2

--- problem2.py
+++ problem2.py
@@ -1,1 +1,16 @@
+'''
+-----------------
+Name: problem2.py
+Purpose: Calculates the number of chickens shared between students and Mr. Fabroa
 
+Author: Rishi K
+
+Created: date 07/12/2020
+----------------
+'''
+#create an input for the user to type the amount of students and chicken there are
+students = int(input("How many students are in the class?: "))
+chicken = int(input("How many pieces of chicken are there?: "))
+#print the amount of chicken that the children will get as well as Mr. Fabroa
+print("Each student will get",chicken // students, "pieces of chicken")
+print("Mr. Fabroa gets",chicken % students,"pieces of chicken")
